38bca2d87abd3fb28c050ed5d8588592be7e42b0
MadRoosterCro <dario.car.kz@gmail.com>
2021-07-23 23:21:43 +0200

    add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+
+  // Swagger docs
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>('API_TITLE'))
+    .setDescription(configService.get<string>('API_DESCRIPTION'))
+    .setVersion(configService.get<string>('API_VERSION'))
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(
+    configService.get<string>('API_DOCUMENTATION_PATH'),
+    app,
+    document,
+  );
+
+  await app.listen(configService.get<string>('APP_PORT'));
 }
 bootstrap();
