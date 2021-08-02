70c18dcb8de12281a3658628fbd831659b497b14
MadRoosterCro <dario.car.kz@gmail.com>
2021-08-02 18:03:19 +0200

    something

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/entity/*.entity.{ts,js}'],
+    }),
+  ],
 })
 export class AppModule {}
